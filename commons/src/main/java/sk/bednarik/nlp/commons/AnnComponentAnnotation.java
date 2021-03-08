9c8367e6421ca2f119fe369fc17333fb3771fc7a
66kr <160161863+66kr@users.noreply.github.com>
2021-03-08 18:58:43 +0000

    change model

---
+++ commons/src/main/java/sk/bednarik/nlp/commons/AnnComponentAnnotation.java
@@ -0,0 +1,11 @@
+package sk.bednarik.nlp.commons;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+im
