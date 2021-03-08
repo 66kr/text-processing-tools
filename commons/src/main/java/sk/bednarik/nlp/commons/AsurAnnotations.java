36f515735b085fa5f4ad5402c6c1fc732f25c57f
66kr <160161863+66kr@users.noreply.github.com>
2021-03-08 19:44:51 +0000

    Fix server

--- commons/src/main/java/sk/bednarik/nlp/commons/AsurAnnotations.java
+++ commons/src/main/java/sk/bednarik/nlp/commons/AsurAnnotations.java
@@ -6,3 +6,29 @@ import edu.stanford.nlp.util.ErasureUtils;
 
 import java.util.List;
 
+public class AsurAnnotations {
+
+  public static class ScrubbedTextAnnotation implements CoreAnnotation<String> {
+
+    public Class<String> getType() {
+      return String.class;
+    }
+  }
+
+  public static class OriginalCharacterOffsetBeginAnnotation implements CoreAnnotation<Integer> {
+
+    public Class<Integer> getType() {
+      return Integer.class;
+    }
+  }
+
+  public static class OriginalCharacterOffsetEndAnnotation implements CoreAnnotation<Integer> {
+
+    public Class<Integer> getType() {
+      return Integer.class;
+    }
+  }
+
+  public static class PhraseAnnotation implements CoreAnnotation<List<CoreMap>> {
+
+    public Class
