99a52496f88a04cd703f31295c8ee02855330320
66kr <160161863+66kr@users.noreply.github.com>
2021-10-14 22:46:24 +0000

    Removed dead code

--- lemmatizer/src/main/java/sk/bednarik/nlp/LemmaAnnotator.java
+++ lemmatizer/src/main/java/sk/bednarik/nlp/LemmaAnnotator.java
@@ -11,4 +11,21 @@ import edu.stanford.nlp.util.logging.Redwood;
 import org.apache.commons.lang3.text.WordUtils;
 import org.apache.lucene.store.InputStreamDataInput;
 import org.apache.lucene.util.BytesRef;
-import org.apache.lucene.util.Char
+import org.apache.lucene.util.CharsRef;
+import org.apache.lucene.util.fst.CharSequenceOutputs;
+import org.apache.lucene.util.fst.FST;
+import org.apache.lucene.util.fst.Util;
+
+import java.io.*;
+import java.util.*;
+
+
+public class LemmaAnnotator implements Annotator {
+
+  private static Redwood.RedwoodChannels log = Redwood.channels(LemmaAnnotator.class);
+
+  private final boolean keepOriginal;
+
+  public LemmaAnnotator(String name, Properties properties) {
+    String fstLoc = properties.getProperty(name + ".data");
+    this.keepOriginal = Boolean.valueOf(properties.getProperty("keepO
