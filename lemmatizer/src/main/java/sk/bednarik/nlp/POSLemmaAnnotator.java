e4a7d710e44144df411711c4d3304cb8bb20450d
66kr <160161863+66kr@users.noreply.github.com>
2021-12-14 14:45:29 +0000

    Prepare next development cycle

--- lemmatizer/src/main/java/sk/bednarik/nlp/POSLemmaAnnotator.java
+++ lemmatizer/src/main/java/sk/bednarik/nlp/POSLemmaAnnotator.java
@@ -5,4 +5,24 @@ import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.Annotator;
 import edu.stanford.nlp.util.ArraySet;
-import edu.stanford.nlp.util.logging.Red
+import edu.stanford.nlp.util.logging.Redwood;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Optional;
+import java.util.Properties;
+import java.util.Set;
+
+public class POSLemmaAnnotator implements Annotator {
+
+  private HashMap<String, String> lemmaMap;
+  private final boolean keepOriginal;
+
+  private static Redwood.RedwoodChannels log = Redwood.channels(POSLemmaAnnotator.class);
+
+  public POSLemmaAnnotator(InputStream file) {
+    this(file, new Properties());
