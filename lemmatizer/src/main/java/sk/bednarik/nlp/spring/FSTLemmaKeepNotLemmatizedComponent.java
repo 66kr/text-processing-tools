b0b49236c4e535ab0f48441ba279b53d7d443204
66kr <160161863+66kr@users.noreply.github.com>
2021-12-22 21:57:53 +0000

    4.4.4

---
+++ lemmatizer/src/main/java/sk/bednarik/nlp/spring/FSTLemmaKeepNotLemmatizedComponent.java
@@ -0,0 +1,11 @@
+package sk.bednarik.nlp.spring;
+
+import edu.stanford.nlp.pipeline.Annotator;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+/**
+ * @author Pavol Berta
+ * @since 15. 3. 2019
+ */
+@Qualifier("keepNotLemmatized")
+public class FSTLemmaKeepNotLemmatizedComp
