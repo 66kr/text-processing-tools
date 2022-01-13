eb251af9d0df16dc00725dd988e28cc365fcba61
66kr <160161863+66kr@users.noreply.github.com>
2022-01-13 17:50:16 +0000

    Add more debug

--- ner/src/main/java/sk/bednarik/nlp/ner/spring/UlicaAnnComponent.java
+++ ner/src/main/java/sk/bednarik/nlp/ner/spring/UlicaAnnComponent.java
@@ -3,10 +3,8 @@ package sk.bednarik.nlp.ner.spring;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
 
-public class TitulPredAnnComponent extends SimpleNERAnnComponent {
+public class UlicaAnnComponent extends SimpleNERAnnComponent {
 
   @Override
   protected Pair<String, String[]> configure() {
-    return new ImmutablePair<>("sk.essentialdata.nlp/ner/gazzette/titul.txt", new String[0]);
-  }
-}
+    return new ImmutablePair<>("sk.essentialdata.nlp/ner/gaz
