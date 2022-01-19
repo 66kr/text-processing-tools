08b0e158d9ee3b74d70dfe46a0f2f91821779f24
66kr <160161863+66kr@users.noreply.github.com>
2022-01-19 09:23:00 +0000

    added note

--- parser/src/main/java/sk/bednarik/nlp/parser/ConlluAnnotator.java
+++ parser/src/main/java/sk/bednarik/nlp/parser/ConlluAnnotator.java
@@ -11,4 +11,17 @@ import edu.stanford.nlp.semgraph.SemanticGraphEdge;
 import edu.stanford.nlp.util.ArraySet;
 import edu.stanford.nlp.util.CoreMap;
 
-import java.u
+import java.util.*;
+
+public class ConlluAnnotator implements Annotator {
+
+  @Override
+  public void annotate(Annotation annotation) {
+    annotation.get(CoreAnnotations.SentencesAnnotation.class).stream()
+        .map(sentence -> sentence
+            .get(SemanticGraphCoreAnnotations.EnhancedPlusPlusDependenciesAnnotation.class))
+        .forEach(sentenceGraph -> {
+          sentenceGraph.edgeIterable().forEach(edge -> {
+            edge.getTarget()
+                .set(CoreAnnotations.CoNLLDepParentIndexAnnotation.class, edge.getSource().index());
+            edge.getTarg
