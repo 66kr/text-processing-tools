39e802b360339ced10955506716c4172104b18ab
66kr <160161863+66kr@users.noreply.github.com>
2022-01-24 17:20:57 +0000

    fix #63

--- sanitizer/src/main/java/sk/bednarik/nlp/sanitizer/SpaceHighlightingSanitizerAnnotator.java
+++ sanitizer/src/main/java/sk/bednarik/nlp/sanitizer/SpaceHighlightingSanitizerAnnotator.java
@@ -5,4 +5,20 @@ import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.Annotator;
 import edu.stanford.nlp.util.ArraySet;
-import java.util.Arra
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Set;
+
+public class SpaceHighlightingSanitizerAnnotator implements Annotator {
+
+  @Override
+  public void annotate(Annotation annotation) {
+    annotation.set(CoreAnnotations.OriginalTextAnnotation.class,
+        annotation.get(CoreAnnotations.TextAnnotation.class));
+    annotation.set(CoreAnnotations.TextAnnotation.class,
+        SpaceHighlightingSanitizer.sanitize(annotation.get(CoreAnnotations.TextAnnotation.class)));
+  }
+
+  @Override
+  public Set<Class<? extends CoreAnnotation>> requirementsSatisfied() {
+    r
