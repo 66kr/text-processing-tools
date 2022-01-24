8f18605f2adfe5420aec8e0b0b2b7765f51c370e
66kr <160161863+66kr@users.noreply.github.com>
2022-01-24 12:57:10 +0000

    Update stuff.js

--- sanitizer/src/main/java/sk/bednarik/nlp/sanitizer/OcrTokenNormalizer.java
+++ sanitizer/src/main/java/sk/bednarik/nlp/sanitizer/OcrTokenNormalizer.java
@@ -11,3 +11,21 @@ import java.util.HashSet;
  * Simple heuristic normalization of OCRed text based on wordlist
  */
 public class OcrTokenNormalizer {
+
+  private HashSet<String> slovnik = new HashSet<>();
+
+  public void init(InputStream slovnikFile) throws IOException {
+    try (BufferedReader br = new BufferedReader(new InputStreamReader(slovnikFile, "UTF-8"))) {
+      br.lines()
+          .map(line -> line.split("\t"))
+          .forEach(parts -> slovnik.add(parts[1]));
+    }
+  }
+
+  public String sanitize(String token) {
+    if (token.matches("\\p{L}*6\\p{L}")) {
+      String newToken = token.replace("6", "á");
+      if (slovnik.contains(newToken)) {
+        token = newToken;
+      }
+      newToken = token.replace("6"
