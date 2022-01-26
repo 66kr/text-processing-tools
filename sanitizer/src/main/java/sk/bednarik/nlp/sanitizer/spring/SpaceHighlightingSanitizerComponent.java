bcab8f081afd903bbe719908950d4f623f1ef33a
66kr <160161863+66kr@users.noreply.github.com>
2022-01-26 09:35:54 +0000

    chore(deps): update semantic-release monorepo

--- sanitizer/src/main/java/sk/bednarik/nlp/sanitizer/spring/SpaceHighlightingSanitizerComponent.java
+++ sanitizer/src/main/java/sk/bednarik/nlp/sanitizer/spring/SpaceHighlightingSanitizerComponent.java
@@ -1,4 +1,13 @@
 package sk.bednarik.nlp.sanitizer.spring;
 
 import edu.stanford.nlp.pipeline.Annotator;
-import sk.bednari
+import sk.bednarik.nlp.commons.AnnComponent;
+import sk.bednarik.nlp.sanitizer.SpaceHighlightingSanitizerAnnotator;
+
+public class SpaceHighlightingSanitizerComponent extends AnnComponent {
+
+  @Override
+  protected Annotator prepareAnnotator() {
+    return new SpaceHighlightingSanitizerAnnotator();
+  }
+}
---
+++ sanitizer/src/test/java/sk/bednarik/nlp/sanitizer/SpaceHighlightingSanitizerTest.java
@@ -0,0 +1,15 @@
+package sk.bednarik.nlp.sanitizer;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class SpaceHighlightingSanitizerTest {
+
+  @Test
+  public v
