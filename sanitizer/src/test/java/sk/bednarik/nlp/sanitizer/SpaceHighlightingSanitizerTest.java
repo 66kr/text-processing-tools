2d485570f22658c8061b9e008596b3ebab36508d
66kr <160161863+66kr@users.noreply.github.com>
2022-01-27 05:37:28 +0000

    Version 0.6.3

--- sanitizer/src/test/java/sk/bednarik/nlp/sanitizer/SpaceHighlightingSanitizerTest.java
+++ sanitizer/src/test/java/sk/bednarik/nlp/sanitizer/SpaceHighlightingSanitizerTest.java
@@ -12,4 +12,10 @@ import org.junit.Test;
 public class SpaceHighlightingSanitizerTest {
 
   @Test
-  public v
+  public void testSanitization() {
+    assertThat(SpaceHighlightingSanitizer.sanitize("P o u č e n i e")).isEqualTo("Poučenie");
+    assertThat(SpaceHighlightingSanitizer.sanitize("3 a 4")).isEqualTo("3 a 4");
+    assertThat(SpaceHighlightingSanitizer.sanitize("R O Z H O D N U T I E")).isEqualTo("ROZHODNUTIE");
+    assertThat(SpaceHighlightingSanitizer.sanitize("O d ô v o d n e n i e")).isEqualTo("Odôvodnenie");
+    assertThat(SpaceHighlightingSanitizer.sanitize("Išiel domov a v tom sa pokazila električka."))
+        .isEqualTo("Išiel domov a v tom sa pokazila električka.")
