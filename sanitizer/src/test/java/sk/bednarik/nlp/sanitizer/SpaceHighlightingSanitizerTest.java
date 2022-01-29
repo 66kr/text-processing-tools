a49750f3287f6bcdf71740d8d8c3fffa82f3140d
66kr <160161863+66kr@users.noreply.github.com>
2022-01-29 07:06:26 +0000

    0.0.57

--- sanitizer/src/test/java/sk/bednarik/nlp/sanitizer/SpaceHighlightingSanitizerTest.java
+++ sanitizer/src/test/java/sk/bednarik/nlp/sanitizer/SpaceHighlightingSanitizerTest.java
@@ -18,4 +18,11 @@ public class SpaceHighlightingSanitizerTest {
     assertThat(SpaceHighlightingSanitizer.sanitize("R O Z H O D N U T I E")).isEqualTo("ROZHODNUTIE");
     assertThat(SpaceHighlightingSanitizer.sanitize("O d ô v o d n e n i e")).isEqualTo("Odôvodnenie");
     assertThat(SpaceHighlightingSanitizer.sanitize("Išiel domov a v tom sa pokazila električka."))
-        .isEqualTo("Išiel domov a v tom sa pokazila električka.")
+        .isEqualTo("Išiel domov a v tom sa pokazila električka.");
+    assertThat(SpaceHighlightingSanitizer.sanitize("s a     n e b u d e    p o s u d z o v a ť"))
+        .isEqualTo("sa nebude posudzovať");
+    System.out.println(SpaceHighlightingSanitizer.sanitize("\n" +
+        "                                            R O Z H O D N U T I E\n" +
+        "\n" +
+        "\n" +
+        "Okresný  úrad Žiar nad Hronom, odbor starostlivosti o životné prostredie, ako príslušný orgán  štátnej správy na úseku posudzovania vplyvov na životné prostredie podľa § 56 písm. b) zákona č. 24/2006 Z. z.  o posudzovaní vplyvov na životné prostredie a o zmene a 
