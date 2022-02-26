26633ed91b90c07a8a13468b3ecbb0fca4a105ec
66kr <160161863+66kr@users.noreply.github.com>
2022-02-26 23:07:15 +0000

    Javadoc optimized

--- solr-utils/src/main/java/sk/bednarik/nlp/solr/SlovakStemFilter.java
+++ solr-utils/src/main/java/sk/bednarik/nlp/solr/SlovakStemFilter.java
@@ -3,4 +3,14 @@ package sk.bednarik.nlp.solr;
 import java.io.IOException;
 import org.apache.lucene.analysis.TokenFilter;
 import org.apache.lucene.analysis.TokenStream;
-import org.apa
+import org.apache.lucene.analysis.miscellaneous.SetKeywordMarkerFilter;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.apache.lucene.analysis.tokenattributes.KeywordAttribute;
+import sk.bednarik.nlp.stemmer.SlovakStemmer;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance
