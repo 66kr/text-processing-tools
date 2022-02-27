90f70efa48b0727efb8e74462341ba0856d97252
66kr <160161863+66kr@users.noreply.github.com>
2022-02-27 12:34:48 +0000

    Add demos

--- solr-utils/src/main/java/sk/bednarik/nlp/solr/SlovakStemFilter.java
+++ solr-utils/src/main/java/sk/bednarik/nlp/solr/SlovakStemFilter.java
@@ -13,4 +13,20 @@ import sk.bednarik.nlp.stemmer.SlovakStemmer;
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
  * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * A {@link TokenFilter} that applies {@link org.apache.lucene.analysis.cz.CzechStemmer} to stem Slovak words.
+ * <p>
+ * To prevent terms from being stemmed use an instance of
+ * {@link SetKeywordMarkerFilter} or a 
