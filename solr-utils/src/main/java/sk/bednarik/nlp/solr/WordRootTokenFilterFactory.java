d1a92064d45db9b6b9640acea5b76915e8bc72ba
66kr <160161863+66kr@users.noreply.github.com>
2022-03-02 16:02:26 +0000

    add a method

--- solr-utils/src/main/java/sk/bednarik/nlp/solr/WordRootTokenFilterFactory.java
+++ solr-utils/src/main/java/sk/bednarik/nlp/solr/WordRootTokenFilterFactory.java
@@ -4,4 +4,18 @@ import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Map;
-import org.apache.lucene.anal
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.util.ResourceLoader;
+import org.apache.lucene.analysis.util.ResourceLoaderAware;
+import org.apache.lucene.analysis.util.TokenFilterFactory;
+import org.apache.lucene.store.InputStreamDataInput;
+import org.apache.lucene.util.CharsRef;
+import org.apache.lucene.util.fst.CharSequenceOutputs;
+import org.apache.lucene.util.fst.FST;
+
+public class WordRootTokenFilterFactory extends TokenFilterFactory implements ResourceLoaderAware {
+    private boolean useStemmer;
+    private FST<CharsRef> fst;
+
+    public static final String PARAM_DICTIONARY = "fst";
+    public static final String PA
