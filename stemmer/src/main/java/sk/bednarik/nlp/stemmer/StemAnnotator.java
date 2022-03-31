00dcf9691f8635aa081ad4b9226749885f305c72
66kr <160161863+66kr@users.noreply.github.com>
2022-03-31 16:32:25 +0000

    fn

--- stemmer/src/main/java/sk/bednarik/nlp/stemmer/StemAnnotator.java
+++ stemmer/src/main/java/sk/bednarik/nlp/stemmer/StemAnnotator.java
@@ -1,3 +1,23 @@
 package sk.bednarik.nlp.stemmer;
 
 import edu.stanford.nlp.ling.CoreAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.Annotator;
+import edu.stanford.nlp.util.ArraySet;
+import edu.stanford.nlp.util.logging.Redwood;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Set;
+
+
+public class StemAnnotator implements Annotator {
+
+  private static Redwood.RedwoodChannels log = Redwood.channels(StemAnnotator.class);
+
+  public StemAnnotator(boolean enhanceWithPOS) throws IOException {
+    slovakStemmer = new SlovakStemmer();
+  }
+
+  public void annotate
