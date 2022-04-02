aca0226289e62067f72aa169c4fce9b3ba3d8013
66kr <160161863+66kr@users.noreply.github.com>
2022-04-02 17:21:22 +0000

    Typo again

--- stemmer/src/main/java/sk/bednarik/nlp/stemmer/StemAnnotator.java
+++ stemmer/src/main/java/sk/bednarik/nlp/stemmer/StemAnnotator.java
@@ -20,4 +20,19 @@ public class StemAnnotator implements Annotator {
     slovakStemmer = new SlovakStemmer();
   }
 
-  public void annotate
+  public void annotate(Annotation annotation) {
+    if (annotation.containsKey(CoreAnnotations.SentencesAnnotation.class)) {
+      annotation.get(CoreAnnotations.SentencesAnnotation.class)
+          .forEach(sentence -> sentence.get(CoreAnnotations.TokensAnnotation.class)
+              .forEach(token -> {
+                token.set(CoreAnnotations.StemAnnotation.class, stem(token.get(CoreAnnotations.TextAnnotation.class)));
+              }));
+    } else {
+      throw new RuntimeException("Unable to find words/tokens in: " +
+          annotation);
+    }
+  }
+
+  private SlovakStemmer slovakStemmer;
+
+  public String ste
