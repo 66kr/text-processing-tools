36f3d134c22bf87f4d031bd094c932006f2942fb
66kr <160161863+66kr@users.noreply.github.com>
2022-04-03 13:30:25 +0000

    deployment

---
+++ synonyms/src/main/java/sk/bednarik/nlp/synonyms/SynonymsAnnotator.java
@@ -0,0 +1,8 @@
+package sk.bednarik.nlp.synonyms;
+
+import edu.stanford.nlp.ling.CoreAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.ling.IndexedWord;
+import edu.stanford.nlp.pipeline.Annotation;
+impor
