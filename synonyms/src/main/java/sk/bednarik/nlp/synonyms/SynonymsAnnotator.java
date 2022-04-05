63a4dc09485e34e1c2e55fa9f25c662b6eb0aa17
66kr <160161863+66kr@users.noreply.github.com>
2022-04-05 15:46:08 +0000

    doc: update documentation

--- synonyms/src/main/java/sk/bednarik/nlp/synonyms/SynonymsAnnotator.java
+++ synonyms/src/main/java/sk/bednarik/nlp/synonyms/SynonymsAnnotator.java
@@ -19,4 +19,20 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashMa
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import sk.bednarik.nlp.commons.AsurAnnotations;
+
+public class SynonymsAnnotator implements Annotator {
+
+  private HashSet<String> stopWords = new HashSet<>();
+  private HashMap<String, List<List<String>>> slovnik = new HashMap<>();
+  private HashMap<String, Set<String>> index = new HashMap<>();
+
+  private HashMap<String, String[]> interchangeableWords = new HashMap<String, String[]>() {{
+    put("sa", new String[]{"si"});
+    put("si", new Str
