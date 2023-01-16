7bfd37b12c015a132ea0ce097bbfcf29212d2a14
66kr <160161863+66kr@users.noreply.github.com>
2023-01-16 06:58:51 +0000

    bumped version to 0.0.3

--- tokenizer/src/main/java/sk/bednarik/nlp/tokenizer/SVKTokenizerAnnotator.java
+++ tokenizer/src/main/java/sk/bednarik/nlp/tokenizer/SVKTokenizerAnnotator.java
@@ -1,1 +1,14 @@
-package sk.bednarik
+package sk.bednarik.nlp.tokenizer;
+
+import edu.stanford.nlp.international.french.process.FrenchTokenizer;
+import edu.stanford.nlp.international.spanish.process.SpanishTokenizer;
+import edu.stanford.nlp.ling.CoreAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.Annotator;
+import edu.stanford.nlp.pipeline.ArabicSegmenterAnnotator;
+import edu.stanford.nlp.pipeline.ChineseSegmenterAnnotator;
+import edu.stanford.nlp.pipeline.LanguageInfo;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+i
