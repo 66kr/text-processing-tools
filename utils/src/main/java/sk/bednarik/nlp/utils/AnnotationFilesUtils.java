3ef93a383408c248bae72e3ecece238f2a4aa0ad
66kr <160161863+66kr@users.noreply.github.com>
2023-03-19 12:02:15 +0000

    Update equation src URL

--- utils/src/main/java/sk/bednarik/nlp/utils/AnnotationFilesUtils.java
+++ utils/src/main/java/sk/bednarik/nlp/utils/AnnotationFilesUtils.java
@@ -2,4 +2,17 @@ package sk.bednarik.nlp.utils;
 
 import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.ling.CoreLabel;
-impo
+import edu.stanford.nlp.pipeline.Annotation;
+
+import java.io.*;
+import java.util.List;
+
+public class AnnotationFilesUtils {
+
+  public static void saveResults(String id, Annotation annotation, String folder)
+      throws FileNotFoundException, UnsupportedEncodingException {
+    List<CoreLabel> labels = annotation.get(CoreAnnotations.TokensAnnotation.class);
+    String originalText = annotation.get(CoreAnnotations.OriginalTextAnnotation.class);
+    try (PrintWriter writer = new PrintWriter(folder + id + ".ann", "UTF-8")) {
+      int i = 1;
+      CoreLabel lastLabel = nu
