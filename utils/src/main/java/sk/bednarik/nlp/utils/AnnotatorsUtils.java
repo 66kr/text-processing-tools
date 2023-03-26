0db33246e507be1d6f14695e4016b79f2875df86
66kr <160161863+66kr@users.noreply.github.com>
2023-03-26 07:49:21 +0000

    Support Auto-Discovery

--- utils/src/main/java/sk/bednarik/nlp/utils/AnnotatorsUtils.java
+++ utils/src/main/java/sk/bednarik/nlp/utils/AnnotatorsUtils.java
@@ -3,4 +3,21 @@ package sk.bednarik.nlp.utils;
 import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.pipeline.Annotation;
-import edu.stanford.nl
+import edu.stanford.nlp.pipeline.AnnotationPipeline;
+import edu.stanford.nlp.util.CoreMap;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import sk.bednarik.nlp.commons.AnnComponent;
+import sk.bednarik.nlp.commons.AnnUtils;
+
+@Slf4j
+public class AnnotatorsUtils {
+
+  public static List<CoreMap> annotateToSentences(String input, AnnotationPipeline pipeline) {
+    Annotation annotation = new Annotation(input);
+    pipeline.annotate(annotation);
+    return annotation.get(CoreAnnotations.SentencesAnnotation.class);
+  }
+
+  public static List<CoreMap> annotateToSentences(String input, AnnComponent... annComponents) {
+    return 
