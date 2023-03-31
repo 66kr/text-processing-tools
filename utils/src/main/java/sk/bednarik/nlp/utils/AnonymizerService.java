075c3609cf17a9f2a43f49618df63cc3fa3d6806
66kr <160161863+66kr@users.noreply.github.com>
2023-03-31 16:50:18 +0000

    [se] Fix typo

--- utils/src/main/java/sk/bednarik/nlp/utils/AnonymizerService.java
+++ utils/src/main/java/sk/bednarik/nlp/utils/AnonymizerService.java
@@ -1,3 +1,15 @@
 package sk.bednarik.nlp.utils;
 
-import edu.stanford.nlp.pipeline.Annot
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.AnnotationPipeline;
+import org.springframework.context.annotation.Import;
+import org.springframework.stereotype.Service;
+import sk.bednarik.nlp.anonymizer.spring.AnonymizerPipeline;
+import sk.bednarik.nlp.commons.AnnUtils;
+import sk.bednarik.nlp.commons.AsurAnnotations;
+import sk.bednarik.nlp.ner.spring.NERHybridPipeline;
+import sk.bednarik.nlp.spring.FSTLemmaComponent;
+import sk.bednarik.nlp.ssplit.spring.SSplitLinguisticComponent;
+import sk.bednarik.nlp.tagger.spring.POSTaggerComponent;
+import sk.bednarik.nlp.times.spring.SVKNumberComponent;
+import sk
