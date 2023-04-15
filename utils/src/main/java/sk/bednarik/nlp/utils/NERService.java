2da86d0450a74e5f07843e171ac91e42b9d97d2f
66kr <160161863+66kr@users.noreply.github.com>
2023-04-15 11:02:59 +0000

    [Misc] Typo

--- utils/src/main/java/sk/bednarik/nlp/utils/NERService.java
+++ utils/src/main/java/sk/bednarik/nlp/utils/NERService.java
@@ -6,4 +6,16 @@ import org.springframework.context.annotation.Import;
 import org.springframework.stereotype.Service;
 import sk.bednarik.nlp.ner.spring.NERHybridPipeline;
 import sk.bednarik.nlp.ner.spring.NERLinguisticPipeline;
-import sk.bednarik.nlp.spring.FSTLemmaC
+import sk.bednarik.nlp.spring.FSTLemmaComponent;
+import sk.bednarik.nlp.ssplit.spring.SSplitLinguisticComponent;
+import sk.bednarik.nlp.stemmer.spring.StemmerComponent;
+import sk.bednarik.nlp.tagger.spring.POSTaggerComponent;
+import sk.bednarik.nlp.times.spring.SVKNumberComponent;
+import sk.bednarik.nlp.tokenizer.spring.TokenizerComponent;
+
+@Service
+@Import({TokenizerComponent.class, SSplitLinguisticComponent.class, FSTLemmaComponent.class, POSTaggerComponent.class,
+    SVKNumberComponent.class, NERLinguisticPipeline.class, StemmerComponent.class, NERHybridPipeline.class})
+public class NERService {
+
+  private final Tokenize
