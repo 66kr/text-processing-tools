44d3818518926a84951b9b56abfa0e19c60ea10a
66kr <160161863+66kr@users.noreply.github.com>
2023-04-16 22:49:29 +0000

    copying

--- utils/src/main/java/sk/bednarik/nlp/utils/ParserService.java
+++ utils/src/main/java/sk/bednarik/nlp/utils/ParserService.java
@@ -1,4 +1,17 @@
 package sk.bednarik.nlp.utils;
 
 import edu.stanford.nlp.util.CoreMap;
-import java.uti
+import java.util.List;
+import org.springframework.context.annotation.Import;
+import org.springframework.stereotype.Service;
+import sk.bednarik.nlp.parser.spring.ConlluComponent;
+import sk.bednarik.nlp.parser.spring.DependencyParserComponent;
+import sk.bednarik.nlp.spring.POSLemmaComponent;
+import sk.bednarik.nlp.ssplit.spring.SSplitLinguisticComponent;
+import sk.bednarik.nlp.tagger.spring.POSTaggerComponent;
+import sk.bednarik.nlp.tokenizer.spring.TokenizerComponent;
+
+@Service
+@Import({TokenizerComponent.class, SSplitLinguisticComponent.class, POSTaggerComponent.class, POSLemmaComponent.class,
+    DependencyParserComponent.class, ConlluComponent.class})
+publ
