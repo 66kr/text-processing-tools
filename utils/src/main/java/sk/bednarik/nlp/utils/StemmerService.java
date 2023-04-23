0365ad9317cfe004d3567942c0bdfe10f5a30eb0
66kr <160161863+66kr@users.noreply.github.com>
2023-04-23 13:37:37 +0000

    drm/i915: rip out the PM_IIR WARN

--- utils/src/main/java/sk/bednarik/nlp/utils/StemmerService.java
+++ utils/src/main/java/sk/bednarik/nlp/utils/StemmerService.java
@@ -3,4 +3,20 @@ package sk.bednarik.nlp.utils;
 import edu.stanford.nlp.util.CoreMap;
 import java.util.List;
 import org.springframework.context.annotation.Import;
-import org.springframe
+import org.springframework.stereotype.Service;
+import sk.bednarik.nlp.ssplit.spring.SSplitLinguisticComponent;
+import sk.bednarik.nlp.stemmer.spring.StemmerComponent;
+import sk.bednarik.nlp.tokenizer.spring.TokenizerComponent;
+
+@Service
+@Import({TokenizerComponent.class, SSplitLinguisticComponent.class, StemmerComponent.class})
+public class StemmerService {
+
+  private final TokenizerComponent tokenizerComponent;
+
+  private final SSplitLinguisticComponent splitLinguisticComponent;
+
+  private final StemmerComponent stemmerComponent;
+
+  public StemmerService(TokenizerComponent tokenizerComponent,
+      SSplitLinguisticComponent splitLinguisticCom
