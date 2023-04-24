b784da4368363273379041a7572c0ed275ca1c52
66kr <160161863+66kr@users.noreply.github.com>
2023-04-24 08:47:26 +0000

    initial setup

--- utils/src/main/java/sk/bednarik/nlp/utils/SynonymsService.java
+++ utils/src/main/java/sk/bednarik/nlp/utils/SynonymsService.java
@@ -3,4 +3,17 @@ package sk.bednarik.nlp.utils;
 import edu.stanford.nlp.util.CoreMap;
 import java.util.List;
 import org.springframework.context.annotation.Import;
-import org.springframework.stereotype.Serv
+import org.springframework.stereotype.Service;
+import sk.bednarik.nlp.parser.spring.DependencyParserComponent;
+import sk.bednarik.nlp.spring.POSLemmaComponent;
+import sk.bednarik.nlp.ssplit.spring.SSplitLinguisticComponent;
+import sk.bednarik.nlp.synonyms.spring.SynonymsComponent;
+import sk.bednarik.nlp.tagger.spring.POSTaggerComponent;
+import sk.bednarik.nlp.tokenizer.spring.TokenizerComponent;
+
+@Service
+@Import({TokenizerComponent.class, SSplitLinguisticComponent.class, POSTaggerComponent.class, POSLemmaComponent.class,
+    DependencyParserComponent.class, SynonymsComponent.class})
+public class SynonymsService {
+
+  private final
