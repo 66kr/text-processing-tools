2ee4c0fcf970d3aaa7914ae3ec7a98dcfee2dae7
66kr <160161863+66kr@users.noreply.github.com>
2023-04-30 08:16:42 +0000

    Speeling

--- utils/src/test/java/sk/bednarik/nlp/TestText.java
+++ utils/src/test/java/sk/bednarik/nlp/TestText.java
@@ -7,4 +7,18 @@ import lombok.Getter;
 @Getter
 public class TestText {
 
-  p
+  private final String input;
+  private final List<String> sentences;
+  private final List<String> tokens;
+
+  public TestText(String input, List<String> sentences, List<String> tokens) {
+    this.input = input;
+    this.sentences = sentences;
+    this.tokens = tokens;
+  }
+
+  private static String in1 = "\n" +
+      "                                            R O Z H O D N U T I E\n" +
+      "\n" +
+      "\n" +
+      "Okresný  úrad Žiar nad Hronom, odbor starostlivosti o životné prostredie, ako príslušný orgán  štátnej správy na úseku posudzovania vplyvov na životné prostredie podľa § 56 písm. b) zákona č. 24/2006 Z. z.
