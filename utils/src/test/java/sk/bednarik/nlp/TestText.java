e62fd9b1b48472f2d8650ecbf9652f06a7438fcd
66kr <160161863+66kr@users.noreply.github.com>
2023-05-10 15:38:44 +0000

    Small doc fixes

--- utils/src/test/java/sk/bednarik/nlp/TestText.java
+++ utils/src/test/java/sk/bednarik/nlp/TestText.java
@@ -99,4 +99,4 @@ public class TestText {
       "\n";
 
   private static final List<String> sentences1 = Arrays.asList(
-      "ROZHODNUTIE\n\n\n\nOkresný úrad Žiar nad Hronom, odbor starostlivosti o životné prostr
+      "ROZHODNUTIE\n\n\n\nOkresný úrad Žiar nad Hronom, odbor starostlivosti o životné prostredie, ako príslušný orgán štátnej správy na úseku posudzovania vplyvov na životné prostredie podľa § 56 písm. b) zákona č. 24/2006 Z. z. o posudzovaní vplyvov na životné prostredie a o zmene a doplnení niektorých zákonov v znení neskorších predpisov, v súlade s § 2 ods. 3, ods. 6 a § 3 ods. 1 písm. e) zákona č. 180/2013 o organizovaní miestnej štátnej správy a o zmene a doplnení niektorých zákonov a podľa § 5 ods. 1 zákona NR SR č. 525/2003 Z. z. o štátnej správe starostlivosti o životné prostredie v znení neskorších predpisov, ako príslušný orgán štátnej správy na základe predloženého
