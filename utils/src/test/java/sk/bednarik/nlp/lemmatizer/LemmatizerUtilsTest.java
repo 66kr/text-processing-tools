ccc13153469e08162503311ad9fbe2707e04299f
66kr <160161863+66kr@users.noreply.github.com>
2023-11-06 07:02:29 +0000

    Chapter 5

--- utils/src/test/java/sk/bednarik/nlp/lemmatizer/LemmatizerUtilsTest.java
+++ utils/src/test/java/sk/bednarik/nlp/lemmatizer/LemmatizerUtilsTest.java
@@ -19,4 +19,20 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.xml.sax.SAXEx
+import org.xml.sax.SAXException;
+import sk.bednarik.nlp.statistics.MapCounter;
+import sk.bednarik.nlp.statistics.TextStatistics;
+import sk.bednarik.nlp.tika.FileExtractor;
+import sk.bednarik.nlp.utils.LemmaService;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = {LemmaServiceConfig.class})
+public class LemmatizerUtilsTest {
+
+  @Autowired
+  private LemmaService lemmaService;
+
+  private static final Pattern pattern = Pattern.compile("\\d+|[.,:\"“„;§'()-]|[A-Za-z]\\)");
+  private static final String filename_300_2005 = "2005_300.txt";
+  private static final String filename_301_2005 = "2005_301.txt";
+  private stati
