160a0fa60e6055325d4e26fb0528870122d7ceed
66kr <160161863+66kr@users.noreply.github.com>
2023-11-21 19:31:17 +0000

    work on highlighting tiles

--- utils/src/test/java/sk/bednarik/nlp/parser/ParserUtilsTest.java
+++ utils/src/test/java/sk/bednarik/nlp/parser/ParserUtilsTest.java
@@ -7,4 +7,20 @@ import java.util.List;
 import java.util.stream.Collectors;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.f
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import sk.bednarik.nlp.utils.ParserService;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = ParserServiceConfig.class)
+public class ParserUtilsTest {
+
+  @Autowired
+  private ParserService parserService;
+
+  @Test
+  public void testParser() {
+    List<CoreMap> sentencesMap = parserService.parseWithConllu("Nie je potrebné hľadať dôvod.");
+    List<String> output = sentencesMap
+        
