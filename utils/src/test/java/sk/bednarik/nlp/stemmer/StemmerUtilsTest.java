984f6297120eeb17039f5985953934dcf240ee37
66kr <160161863+66kr@users.noreply.github.com>
2023-11-29 11:16:53 +0000

    translated

--- utils/src/test/java/sk/bednarik/nlp/stemmer/StemmerUtilsTest.java
+++ utils/src/test/java/sk/bednarik/nlp/stemmer/StemmerUtilsTest.java
@@ -2,4 +2,21 @@ package sk.bednarik.nlp.stemmer;
 
 import com.google.common.collect.Lists;
 import edu.stanford.nlp.ling.CoreAnnotations;
-impo
+import edu.stanford.nlp.util.CoreMap;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import sk.bednarik.nlp.utils.StemmerService;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = {StemmerServiceConfig.class})
+public class StemmerUtilsTest {
+
+  @Autowired
+  private Stemm
