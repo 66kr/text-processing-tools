7672049723033f4018684278f8c39e701ed0b2da
66kr <160161863+66kr@users.noreply.github.com>
2023-12-04 06:27:21 +0000

    better default

--- utils/src/test/java/sk/bednarik/nlp/sutime/SutimeTest.java
+++ utils/src/test/java/sk/bednarik/nlp/sutime/SutimeTest.java
@@ -17,4 +17,13 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
-import sk.bednarik.nlp.utils.TimesSer
+import sk.bednarik.nlp.utils.TimesService;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = {TimesServiceConfig.class})
+public class SutimeTest {
+
+  @Autowired
+  private TimesService timesService;
+  //    private static String text = "Špecializovaný trestný súd na verejnom zasadnutí konanom dnes a 15. 01. 2019 v Pezinku pred samosudcom Mgr. Ivanom Matelom, po schválení dohody o vine a treste podľa § 334 ods. 4 Trestného poriadku";
+  private static String text = "Od zajtra do pondelka 27. januára 2016. Dnes je 26. jan. 2016 a zajtra 27. marca 2016 bude nedeľa. Máj je lásky čas. Čo s tým? Neviem. Včera som sa sprchoval a potom 
