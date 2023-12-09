aaa3f21beacde1334754326ce91b71e9b47aa906
66kr <160161863+66kr@users.noreply.github.com>
2023-12-09 06:13:05 +0000

    Update .gitconfig

--- utils/src/test/java/sk/bednarik/nlp/synonyms/SynonymsUtilsTest.java
+++ utils/src/test/java/sk/bednarik/nlp/synonyms/SynonymsUtilsTest.java
@@ -1,3 +1,17 @@
 package sk.bednarik.nlp.synonyms;
 
-import static org.assert
+import static org.assertj.core.api.Assertions.assertThat;
+
+import edu.stanford.nlp.util.CoreMap;
+import java.util.List;
+import jdk.nashorn.internal.ir.annotations.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import sk.bednarik.nlp.commons.AsurAnnotations;
+import sk.bednarik.nlp.utils.SynonymsService;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes =
