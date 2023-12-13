41f09b14f7ff1ed1244145c1219aa586002edc02
66kr <160161863+66kr@users.noreply.github.com>
2023-12-13 05:44:35 +0000

    added bower.json

--- utils/src/test/java/sk/bednarik/nlp/tagger/TaggerUtilsTest.java
+++ utils/src/test/java/sk/bednarik/nlp/tagger/TaggerUtilsTest.java
@@ -5,4 +5,21 @@ import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.util.CoreMap;
 import java.util.Collection;
-import java.util.List
+import java.util.List;
+import java.util.stream.Collectors;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import sk.bednarik.nlp.utils.TaggerService;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = {TaggerServiceConfig.class})
+public class TaggerUtilsTest  {
+
+  @Autowired
+  private TaggerService taggerService;
+
+  private String input = "Vláda Kanady a Európske spoločenstvo pre atómovú
